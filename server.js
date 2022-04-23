4448cbea03bf3ce14cc1ba3ee5e80665de23b469
Glitch (foil-hazel-scene) <none>
2022-04-23 04:38:21 +0000

    ☄️🚝 Checkpoint ./server.js:44143114/926

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  res.send('Hi, I am a http express server from Node.js' );
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${server.address().port}`);
+});
+server.on('error', error => console.log(`Error: ${error.message}`));
+
+app.get('/', (req, res) => {
+    res.send(`<h1 style="color:blue;" >Welcome to express server!</h1>`);
 });
 
-const PORT = process.env.PORT || 3000
+let visitas = 0;
+app.get('/visitas', (req, res) => {
+    res.send(`La cantidad de visitas es: ${++visitas}`);
+});
 
-server.listen(PORT, () => {
-  console.log(`Http server listen ${PORT}`);  
+let fyh = new Date().toLocaleString();
+app.get('/fyh', (req, res) => {
+    res.send(`La fecha y hora actual es: ${fyh}`);
 });
