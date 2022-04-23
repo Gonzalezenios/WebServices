4016dc45f803da08fddd0b9c03c07bfdbf5c7613
Glitch (foil-hazel-scene) <none>
2022-04-23 05:59:20 +0000

    🍙🏟 Checkpoint ./server.js:44143114/505 ./main.js:44143114/922

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const Container = require('./container.js')
+const express = require('express');
+const fs = require('fs');
+const { get } = require('http');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${server.address().port}`);
@@ -11,18 +12,12 @@ const server = app.listen(PORT, () => {
 server.on("error", (error) => console.log(`Error: ${error.message}`));
 
 app.get("/", (req, res) => {
-  res.send(`<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/2A75RyXVzzSI2bx4Gj" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p>/product</p><p><`);
+  res.send(`<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/2A75RyXVzzSI2bx4Gj" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p>/product</p><p>/randomProduct</p>`);
 });
 
-let product = new Container('./products.txt');
-
-    console.log(product)
-    async function createProducts(products) {
-        for (const prod of products) {
-            await product.save(prod);
-        }
-    }
 app.get("/product", (req, res) => {
+  contVisitas++;
+  const data = fs.readFile()
   res.send(`Estos son todos los productos disponibles: ${product}`);
 });
 
