056324107aef8134c3fa7f84919215b263fa5dd7
Glitch (glitch-hello-node) <none>
2022-04-23 04:34:12 +0000

    🏙🚵 Checkpoint ./server.js:44143114/7

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const server = http.createServer((req, res) => {
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
-  console.log(`Http server listen `);  
+  console.log(`Http server listen ${PORT}`);  
 });
